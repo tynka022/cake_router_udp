65129a647e62e18c7d1798c0b212673bb00bc7e1
MaciejStander <96832320+MaciejStander@users.noreply.github.com>
2023-06-05 12:13:31 +0200

    Plik konfiguracyjny

--- cake_router_client.py
+++ cake_router_client.py
@@ -3,16 +3,34 @@ import sys
 import random
 
 nodesIPs = [] # lista adresow posredniczacych
-# przyjmowanie argumentow
-if len(sys.argv) < 4:
-    print("Za mala liczba argumentow. Potrzeba przynajmniej 3 argumentow.")
-    print("python cake_router_client.py <ip serwera docelowego> <ip 1-go wezla> <ip 2-go wezla> ... <ip n-go wezla>")
-    sys.exit(1)
-else:
-    serverIP = sys.argv[1] # adres serwera docelowego
-    # wczytywanie adresow wezlow z listy
-    for i in range(2,len(sys.argv)):
-        nodesIPs.append(sys.argv[i])
+# Wczytywanie adresow IP serwerow z pliku konfiguracyjnego
+file_name = "cake_router.conf" # nazwa pliku konfiguracyjnego
+f = open(file_name,"r") # obiekt przchowujacy plik konfiguracyjny do odczytu
+l_nr = 1 # numer linijki w pliku konfiguracyjnym
+for line in f:
+    if line.find('#') == -1: # jesli linijka nie jest komentarzem, to...
+        line = line.split('\n')[0] # usun z linijki znak przejscia do nastepnej linijki '\n'
+        if len(line.split()) > 1: # jesli w linijce jest wiecej niz jeden wyraz, przerwij program
+            print(f"W pliku " + file_name + f" w linijce {l_nr} musi się znajdować dokladnie jeden adres.")
+            exit()
+        if len(line.split()) == 0: # jesli linijka jest pusta, przejdz do nastepnej linii
+            continue
+        # jesli w linii nie ma wyrazu w postaci IP xxx.xxx.xxx.xxx, gdzie xxx to liczba calkowita z przedzialu [0,255], to przerwij program
+        if any(c.isalpha() for c in line) or line.count('.') != 3:
+            print(f"W pliku " + file_name + f" w linijce {l_nr} nie ma podanego adresu IP")
+            exit()
+        for nr in line.split('.'):
+            if int(nr) > 255: # sprawdzanie, czy liczby w adresie IP sa z zakresu [0,255]
+                print(f"W pliku " + file_name + f" w linijce {l_nr} nie ma podanego adresu IP")
+                exit()
+        nodesIPs.append(line)
+    l_nr += 1
+# sprawdzenie, czy zostaly wczytane przynajmniej 3 adresy IP
+if len(nodesIPs) < 3:
+    print("W pliku " + file_name + " jest za malo adresow IP. Wymagane przynajmniej 3 adresy.")
+    exit()
+
+serverIP = nodesIPs[-1]
 
 inp = input("Wpisz wiadomosc do przeslania:\n")
 
@@ -29,8 +47,6 @@ UDPClientSocketSend = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 UDPClientSocketRecv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 UDPClientSocketRecv.bind(('',recvingPort))
 # wysylanie wiadomosci do serwera
-random.shuffle(nodesIPs) # wybor drogi przesylu danych pomiedzy wezlami
-nodesIPs.append(serverIP) # dopisanie serwera docelowego na koncu listy
 firstNodeIP = nodesIPs[0] # adres pierwszego wezla sieci
 nodes = ' '.join(nodesIPs) # zapisane ip wezlow w kolejnosci
 connetcion_code = random.randint(0,255)
